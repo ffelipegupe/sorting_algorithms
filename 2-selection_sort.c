db42abdf6e606bf53c58b74bc23ec96c5a748dab
ffelipegupe <1560@holbertonschool.com>
2020-06-16 23:57:57 +0000

    Edit: styling

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,29 +9,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
-size_t i = 0;
-size_t j, low;
-int actual = 0;
+	size_t i = 0;
+	size_t j, low;
+	int actual = 0;
 
-if (!array || size < 2)
-return;
-while (i < size)
-{
-    low = i;
-    j = 1 + i;
-    while (j < size)
-    {
-        if(array[j] < array[low])
-            low = j;
-        j++;
-    }
-    if (i != low)
-    {
-        actual = array[i];
-        array[i] = array[low];
-        array[low] = actual;
-        print_array(array, size);
-    }
-    i++;
-}
+	if (!array || size < 2)
+		return;
+	while (i < size)
+	{
+		low = i;
+		j = 1 + i;
+		while (j < size)
+		{
+			if (array[j] < array[low])
+				low = j;
+			j++;
+		}
+		if (i != low)
+		{
+			actual = array[i];
+			array[i] = array[low];
+			array[low] = actual;
+			print_array(array, size);
+		}
+		i++;
+	}
 }
