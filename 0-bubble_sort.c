0c3e77ba8932798badfcf32bed28a7d487aee7d4
ffelipegupe <1560@holbertonschool.com>
2020-06-16 22:40:08 +0000

    Edit: styling

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,45 +4,45 @@
  * swapper - function that swaps array elements
  * @a: pointer to element to swap
  * @b: pointer to element to swap
- * 
+ *
  * Return: nothing
  */
 
 void swapper(int *a, int *b)
 {
-        int tmp = *a;
-        *a = *b;
-        *b = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
  * bubble_sort - sorts an array of integers using the Bubble sort algorithm
  * @array: array to sort
  * @size: size
- * 
+ *
  * Return: nothing
  */
 
 void bubble_sort(int *array, size_t size)
 {
-        size_t i, j;
-        int check;
+	size_t i, j;
+	int check;
 
-        if (array == NULL || size < 2)
-            return;
-        for (i = 0; i < size; i++)
-        {
-                check = 1;
-                for (j = 0; j < size - i - 1; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                swapper(&array[j], &array[j + 1]);
-                                check = 0;
-                                print_array(array, size);
-                        }
-                }
-                if (check == 1)
-                        break;
-        }
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		check = 1;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swapper(&array[j], &array[j + 1]);
+				check = 0;
+				print_array(array, size);
+			}
+		}
+		if (check == 1)
+			break;
+	}
 }
