672b73e19759c74c8e1ae1867135420fc2a311c5
ffelipegupe <1560@holbertonschool.com>
2020-06-16 23:25:05 +0000

    Add: adding task 2 files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,37 +8,37 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *actual, *tmp, *noder;
-        int check;
+	listint_t *actual, *tmp, *noder;
+	int check;
 
-        if (list == NULL || (*list)->next == NULL)
-                return;
-        actual = (*list)->next;
-        while (actual != NULL)
-        {
-                tmp = actual;
-                while (tmp->prev != NULL)
-                {
-                        check = 0;
-                        if (tmp->n < tmp->prev->n)
-                        {
-                                if (tmp->prev->prev != NULL)
-                                        tmp->prev->prev->next = tmp;
-                                tmp->prev->next = tmp->next;
-                                noder = tmp->prev->prev;
-                                tmp->prev->prev = tmp;
-                                if (tmp->next != NULL)
-                                        tmp->next->prev = tmp->prev;
-                                tmp->next = tmp->prev;
-                                tmp->prev = noder;
-                                check = 1;
-                                if (tmp->prev == NULL)
-                                        *list = tmp;
-                                print_list(*list);
-                        }
-                        if (check == 0)
-                                tmp = tmp->prev;
-                }
-                actual = actual->next;
-        }
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	actual = (*list)->next;
+	while (actual != NULL)
+	{
+		tmp = actual;
+		while (tmp->prev != NULL)
+		{
+			check = 0;
+			if (tmp->n < tmp->prev->n)
+			{
+				if (tmp->prev->prev != NULL)
+					tmp->prev->prev->next = tmp;
+				tmp->prev->next = tmp->next;
+				noder = tmp->prev->prev;
+				tmp->prev->prev = tmp;
+				if (tmp->next != NULL)
+					tmp->next->prev = tmp->prev;
+				tmp->next = tmp->prev;
+				tmp->prev = noder;
+				check = 1;
+				if (tmp->prev == NULL)
+					*list = tmp;
+				print_list(*list);
+			}
+			if (check == 0)
+				tmp = tmp->prev;
+		}
+		actual = actual->next;
+	}
 }
