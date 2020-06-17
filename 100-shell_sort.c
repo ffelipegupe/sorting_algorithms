d9363ce9106428029bbf258a325a02c3038bc060
ffelipegupe <1560@holbertonschool.com>
2020-06-17 22:44:14 +0000

    Edit: styling

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,4 +1,4 @@
-include "sort.h"
+#include "sort.h"
 
 /**
  * shell_sort - sorts using the Shell sort algorithm/Knuth sequence
@@ -9,26 +9,26 @@ include "sort.h"
  */
 void shell_sort(int *array, size_t size)
 {
-        size_t gap = 1, i, j;
-        int tmp;
+	size_t gap = 1, i, j;
+	int tmp;
 
-        if (!array || size < 2)
-                return;
-        while (gap < size)
-                gap = (gap * 3) + 1
-        gap = (gap - 1) / 3
-        while (gap > 0)
-        {
-                i = gap;
-                while (i < size)
-                {
-                        tmp = array[i];
-                        for (j = i; j >= gap && array[j - gap] > tmp; j -=gap)
-                             array[j] = array[j - gap]
-                        array[j] = tmp;
-                        i++;
-                }
-                print_array(array, size);
-                gap = (gap - 1) / 3;
-        }
+	if (!array || size < 2)
+		return;
+	while (gap < size)
+		gap = (gap * 3) + 1;
+	gap = (gap - 1) / 3;
+	while (gap > 0)
+	{
+		i = gap;
+		while (i < size)
+		{
+			tmp = array[i];
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = tmp;
+			i++;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
 }
