7ae4db22e83fc9667ee0a42e74fb19d420385473
ffelipegupe <1560@holbertonschool.com>
2020-06-17 00:52:10 +0000

    Edit: styling

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -20,45 +20,49 @@ void swapper(int *a, int *b)
  * @array: pointer to the array
  * @low: lower element
  * @high: higher element
+ * @size: size of the array
  *
  * Return: the number
  */
-int partition (int *array, int low, int high, size_t size)
+int partition(int *array, int low, int high, size_t size)
 {
-    int j;
-    int pivot = array[high];
-    int a = (low - 1);
-    
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            a++;
-            swapper(&array[a], &array[j]);
-            print_array(array, size);
-        }
-    }
-    swapper(&array[a + 1], &array[high]);
-    print_array(array, size);
-    return (a + 1);
+	int j;
+	int pivot = array[high];
+	int a = (low - 1);
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			a++;
+			swapper(&array[a], &array[j]);
+			print_array(array, size);
+		}
+	}
+	swapper(&array[a + 1], &array[high]);
+	print_array(array, size);
+	return (a + 1);
 }
 
 /**
  * quick_helper - function to take low and high arguments
  * @array: array to sort
  * @size: size of the array
+ * @low: lowest
+ * @high: highest
  *
  * Return: nothing
  */
 
 void quick_helper(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pa = partition(array, low, high, size);
-        quick_helper(array, low, pa - 1, size);
-        quick_helper(array, pa + 1, high, size);
-    }
+	if (low < high)
+	{
+		int pa = partition(array, low, high, size);
+
+		quick_helper(array, low, pa - 1, size);
+		quick_helper(array, pa + 1, high, size);
+	}
 }
 /**
  * quick_sort - sorts an array of integers using the Quick sort algorithm
@@ -69,10 +73,10 @@ void quick_helper(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    size_t low = 0;
-    size_t high = size - 1;
+	size_t low = 0;
+	size_t high = size - 1;
 
-    if (!array || size < 2)
-        return;
-    quick_helper(array, low, high, size);
+	if (!array || size < 2)
+		return;
+	quick_helper(array, low, high, size);
 }
